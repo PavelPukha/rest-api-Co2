bea6560f18d3682a112d6f72629d1c2227c88b78
PavelPukha <ppukha@gmail.com>
2023-12-20 15:25:13 +0100

    WebApiFetcher

--- src/main/java/WebApiFetcher.java
+++ src/main/java/WebApiFetcher.java
@@ -1,137 +1,9 @@
-//import java.io.*;
-//import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
-//import java.net.ProtocolException;
-//import java.net.URL;
-//
-// class WebApiFetcher {
-//
-//
-//     public static final String API_URL = "https://www.carboninterface.com/api/v1/estimates";
-//     public static final String API_KEY = "T2zfr335K813jQDAOyQJw";
-//
-//
-//
-//
-//
-//     {
-//         URL url = null;
-//         try {
-//             url = new URL(API_URL);
-//         } catch (MalformedURLException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//         HttpURLConnection connection = null;
-//         try {
-//             connection = (HttpURLConnection) url.openConnection();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//
-//
-//         try {
-//             connection.setRequestMethod("POST");
-//         } catch (ProtocolException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//
-//         connection.setRequestProperty("Authorization", "Bearer " + API_KEY);
-//         connection.setRequestProperty("Content-Type","application/json");
-//
-//         connection.setDoOutput(true);
-//         OutputStream outStream = null;
-//         try {
-//             outStream = connection.getOutputStream();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//         OutputStreamWriter outStreamWriter = null;
-//         try {
-//             outStreamWriter = new OutputStreamWriter(outStream, "UTF-8");
-//         } catch (UnsupportedEncodingException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//
-//
-//
-////         outStreamWriter.write(estimateRequestAsString);
-////                outStreamWriter.write(estimateResponseAsString);
-//         try {
-//             outStreamWriter.flush();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//         try {
-//             outStreamWriter.close();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//         try {
-//             outStream.close();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-////
-////            System.out.println(connection.getResponseCode());
-////            System.out.println(connection.getResponseMessage());
-////
-////            InputStream json = connection.getInputStream();
-//
-//
-//         int responseCode = 0;
-//         try {
-//             responseCode = connection.getResponseCode();
-//         } catch (IOException ex) {
-//             throw new RuntimeException(ex);
-//         }
-//
-//         if (responseCode == HttpURLConnection.HTTP_CREATED) {
-//
-//             BufferedReader reader = null;
-//             try {
-//                 reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//             } catch (IOException ex) {
-//                 throw new RuntimeException(ex);
-//             }
-//             StringBuilder response = new StringBuilder();
-//
-//             String line;
-//             while (true) {
-//                 try {
-//                     if (!((line = reader.readLine()) != null)) break;
-//                 } catch (IOException ex) {
-//                     throw new RuntimeException(ex);
-//                 }
-//                 response.append(line).append(System.lineSeparator());
-//             }
-//
-//             try {
-//                 reader.close();
-//             } catch (IOException ex) {
-//                 throw new RuntimeException(ex);
-//             }
-//
-//             String responseData = response.toString();
-//             System.out.println("API Response:");
-//             System.out.println(responseData);
-//
-//         } else {
-//             System.out.println("HTTP Request Failed. Response Code: " + responseCode);
-//         }
-//
-//
-//
-//         connection.disconnect();
-//
-//     }
-//     }
-//
-//
 
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class WebApiFetcher {
 
@@ -150,7 +22,7 @@ public class WebApiFetcher {
 
             connection.setDoOutput(true);
             try (OutputStream outStream = connection.getOutputStream();
-                 OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream, "UTF-8")) {
+                 OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream, StandardCharsets.UTF_8)) {
 
                 outStreamWriter.write(payload);
                 outStreamWriter.flush();
